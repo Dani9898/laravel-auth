fef6ecd70c7716d505abd35312946019c562482a
Dani9898 <danieleoliva98@gmail.com>
2022-02-02 15:58:53 +0100

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    
+    @auth
+
+        <h1>{{ Auth::user() -> name }}</h1>
+
+        <a class="btn btn-secondary" href="{{ route('logout') }}">LOGOUT</a>
+
+    @endauth
+
+    asdasd
+
+@endsection
